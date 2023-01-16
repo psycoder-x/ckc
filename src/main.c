21ed49e9121a196e629210cacdaf73ef935574eb
psycoder-x <psycoderx@gmail.com>
2023-01-16 11:18:25 +0200

    main.c: display help and version
    "-h" "--help" option prints brief documentation for
    how to invoke the program, on standard output.
    
    "-v" "--version" option prints information about the name, version,
    origin, and legal status of the program.
    
    The provided machanism can be used to check if one of these options
    are triggered and then exit the program successfully.
    
    Two TODOs destroyed.
    The argstream example is replaced by the actual usage of the argstream.

--- src/main.c
+++ src/main.c
@@ -3,7 +3,30 @@
  */
 
 #include<stdio.h>
+#include<ctype.h>
 #include"./argstream.h"
+
+/* Spaces between any "information unit" */
+#define GAP_LEN (2)
+/* Length of dash "-" and option alpha together */
+#define SHORT_OPT_LEN (2)
+/* Length of dash "--" before option name */
+#define LONG_OPT_PREFIX_LEN (2)
+/* Maximum length of the option name */
+#define MAX_OPT_NAME_LEN (16)
+
+/* Print the character c to file f, n times */
+static void fputc_n_times(char c, FILE *f, size_t n);
+/* Print alpha then name then info for each option */
+static void print_help(size_t optc, const Opt *optv);
+/* Print information about the name, version, origin, and legal status
+ * of the program
+ */
+static void print_version();
+/* Look for options such as help or version
+ * and print the relevant information
+ */
+static bool handle_info_options(Args *a, size_t optc, const Opt *optv);
 
 int main(int argc, char **argv) {
   /* Available options */
@@ -21,28 +44,75 @@ int main(int argc, char **argv) {
       .info = SV_SZ("Place the output into <file>.")
     },
   };
+  size_t optc = sizeof(optv)/sizeof(Opt);
 
-  /* Temporary example */
-  Args *a = aopen(argc, argv, sizeof(optv)/sizeof(Opt), optv);
-  while (!aeos(a) && !aerror(a)) {
-    const Arg *arg = aget(a);
-    if (arg->alpha != '\0') {
-      printf("-%c ", arg->alpha);
-    }
-    if (!sv_mty(arg->value)) {
-      putchar('<');
-      fwrite(arg->value.data, arg->value.size, 1, stdout);
-      putchar('>');
-    }
-    putchar('\n');
-  }
-  long n = atell(a);
-  printf("atell() = %d\n", n);
+  Args *a = aopen(argc, argv, optc, optv);
+
+  bool handled = handle_info_options(a, optc, optv);
+
+  // TODO: exit the program if any info option has been handled
+  (void)handled;
+
   aclose(a);
-  /* Example end */
-
-  // TODO: handle help option and stop the program
-  // TODO: handle version option and stop the program
 
   return 0;
+}
+
+void fputc_n_times(char c, FILE *f, size_t n) {
+  for (size_t i = 0; i < n; i++) {
+    fputc(c, f);
+  }
+}
+
+void print_help(size_t optc, const Opt *optv) {
+  static const char *message = 
+    "Usage: ckc [options] file...\n"
+    "Options:\n";
+  printf("%s", message);
+  for (size_t i = 0; i < optc; i++) {
+    fputc_n_times(' ', stdout, GAP_LEN);
+    if (isgraph(optv[i].alpha)) {
+      putchar('-');
+      putchar(optv[i].alpha);
+    } else {
+      fputc_n_times(' ', stdout, SHORT_OPT_LEN);
+    }
+    fputc_n_times(' ', stdout, GAP_LEN);
+    // FIXME: name may exceed the max length, which makes "mega-loop"
+    size_t tab_size;
+    if (!sv_mty(optv[i].name)) {
+      fputc_n_times('-', stdout, LONG_OPT_PREFIX_LEN);
+      fwrite(optv[i].name.data, sv_len(optv[i].name), 1, stdout);
+      tab_size = MAX_OPT_NAME_LEN - sv_len(optv[i].name);
+    } else {
+      tab_size = LONG_OPT_PREFIX_LEN + MAX_OPT_NAME_LEN;
+    }
+    fputc_n_times(' ', stdout, tab_size + GAP_LEN);
+    fwrite(optv[i].info.data, sv_len(optv[i].info), 1, stdout);
+    putchar('\n');
+  }
+}
+
+void print_version() {
+  // TODO: correct the message
+  static const char *message = 
+    "ckc 0.0.0\n"
+    "Copyright (C) 2023 psycoder-x\n"
+    "License: MIT <https://spdx.org/licenses/MIT.html>\n";
+  printf("%s\n", message);
+}
+
+bool handle_info_options(Args *a, size_t optc, const Opt *optv) {
+  arewind(a);
+  while (!aeos(a) && !aerror(a)) {
+    const Arg *arg = aget(a);
+    if (arg->alpha == 'h') {
+      print_help(optc, optv);
+      return true;
+    } else if (arg->alpha == 'v') {
+      print_version();
+      return true;
+    }
+  }
+  return false;
 }
